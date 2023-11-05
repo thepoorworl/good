86d234b43ebbcd3a3072c73ff55b5da6afea5df0
thepoorworl <105717675+thepoorworl@users.noreply.github.com>
2023-11-05 18:54:06 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 export NEZHA_SERVER=${NEZHA_SERVER:-''}
-export NEZHA_PORT=${NEZHA_PORT:-''}
+export NEZHA_PORT=${NEZHA_PORT:-'443'}
 export NEZHA_KEY=${NEZHA_KEY:-''}
 export TLS=${TLS:-'1'}
 export ARGO_DOMAIN=${ARGO_DOMAIN:-''}
 export WEB_DOMAIN=${WEB_DOMAIN:-'example.com'}
 export ARGO_AUTH=${ARGO_AUTH:-''}
-export CFIP=${CFIP:-'icook.hk'}
-export NAME=${NAME:-''}
+export CFIP=${CFIP:-'autocdn1.turbojerry.eu.org'}
+export NAME=${NAME:-'Glitch-good'}
 export SERVER_PORT="${SERVER_PORT:-${PORT:-3000}}"
 
 z="
